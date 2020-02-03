5c82b1acf5641718eb523cbe5075a82bea294fe3
Andreea Vasiliu <andr77c6@edu.easj.dk>
2020-02-03 13:07:09 +0100

    changes

--- EventMakerSingleton/EventMakerSingleton/ViewModel/EventViewModel.cs
+++ EventMakerSingleton/EventMakerSingleton/ViewModel/EventViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventMakerSingleton.Model;
 
 namespace EventMakerSingleton.ViewModel
 {
     class EventViewModel
     {
-        
+        public EventViewModel(EventCatalogSingleton catalog)
+        {
+            Eventcatalog = catalog;
+        }
+
+        public EventCatalogSingleton Eventcatalog { get; set; }
     }
 }
