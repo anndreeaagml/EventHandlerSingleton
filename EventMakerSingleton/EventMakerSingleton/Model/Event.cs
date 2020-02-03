37cf602ec4a628c240bbef1c2df1d46b9aa42085
Andreea Vasiliu <andr77c6@edu.easj.dk>
2020-02-03 12:37:03 +0100

    Fiew

--- EventMakerSingleton/EventMakerSingleton/Model/Event.cs
+++ EventMakerSingleton/EventMakerSingleton/Model/Event.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace EventMakerSingleton.Model
 {
-    class Event
+    public class Event
     {
+        public Event(int id, string name, string description, string place, DateTime dateTime)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Place = place;
+            DateTime = dateTime;
+        }
+
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Description: {Description}, Place: {Place}, Date: {DateTime}";
+        }
     }
 }
---
+++ EventMakerSingleton/EventMakerSingleton/Model/EventCatalogSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMakerSingleton.Model
+{
+    class EventCatalogSingleton
+    {
+        private static EventCatalogSingleton _instance;
+        private ObservableCollection<Event> EventCatalog;
+
+        private EventCatalogSingleton()
+        {
+            EventCatalog=new ObservableCollection<Event>();
+        }
+
+        public static EventCatalogSingleton Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
+                else
+                {
+                    _instance=new EventCatalogSingleton();
+                    return _instance;
+                }
+            }
+        }
+
+        public void Add(Event e)
+        {
+            EventCatalog.Add(e);
+        }
+    }
+}
